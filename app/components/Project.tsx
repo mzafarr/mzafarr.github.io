29d561a27d258a0e12f3fec4dadb5e509c16eb99
Muhammad Zafar <mzafar611@gmail.com>
2023-06-10 14:46:15 +0500

    commit

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -22,18 +22,22 @@ const Project: FC<IProject> = ({
         side === 1 && "flex-row-reverse"
       }`}
     >
-      <div className="flex flex-col flex-1 text-xl">
-        <h2 className="text-3xl font-semibold mb-3">{heading}</h2>
-        <p className="text-lg text-justify ">{desc}</p>
-        <button className="px-6 py-3 text-center bg-violet-700 hover:bg-violet-600 text-white mt-5  mb-2 ml-0 rounded-lg">
-          <a href={link}>View Project</a>
-        </button>
-        <button className="px-6 py-3 text-center bg-slate-700 hover:bg-slate-600 text-white  mt-4 rounded-lg">
-          <a href={code}>Source Code</a>
-        </button>
+      <div className="flex flex-col justify-between flex-1 text-xl">
+        <div>
+          <h2 className="text-3xl font-semibold mb-3">{heading}</h2>
+          <p className="text-lg text-justify ">{desc}</p>
+        </div>
+        <div className="flex flex-col w-full">
+          <button className="px-6 py-3 text-center bg-violet-700 hover:bg-violet-600 text-white mt-5  mb-1.5 ml-0 rounded-lg">
+            <a href={link}>View Project</a>
+          </button>
+          <button className="px-6 py-3 text-center bg-slate-700 hover:bg-slate-600 text-white  mt-2 rounded-lg">
+            <a href={code}>Source Code</a>
+          </button>
+        </div>
       </div>
       <div className="flex-1">
-        <Image src={snapshot} alt={heading} width={500} height={350} />
+        <Image src={snapshot} alt={heading} height={450} />
       </div>
     </div>
   );
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Project from "./Project";
 import chatbot from "../images/chatbot.gif";
-import passwordManager from "../images/passwordManager.gif";
+import chatapp from "../images/chatapp.gif";
+import tesla from "../images/tesla.gif";
+import passwordManager from "../images/pw.gif";
 import { StaticImageData } from "next/image";
 
 interface IProject {
@@ -17,22 +19,41 @@ const Projects = () => {
     {
       heading: "Password Manager Website",
       snapshot: passwordManager,
-      desc: "Password Manager Web App built with MERN stack. Users can create their accounts. I have implemented JSON Web Tokens (JWT) for user authentication, ensuring enhanced security with encrypted password storage.",
-      link: "google.com",
-      code: "github.com",
+      desc: "Password Manager Web App built with MERN stack. Users can create their accounts. I have implemented JSON Web Tokens for user authentication, ensuring enhanced security with encrypted password storage.",
+      code: "https://github.com/mzafarr/PasswordMangerFront",
+      link: "https://password-manger-front.vercel.app/",
       side: 1,
+    },
+    {
+      heading: "Chat Application",
+      snapshot: chatapp,
+      desc: "Built a real-time chat app using Next.js and firebase. Integrated Firebase Authentication for secure user authentication.",
+      code: "https://github.com/mzafarr/chat-app",
+      link: "https://chat-app-wheat-beta.vercel.app/",
+      side: 0,
     },
     {
       heading: "Custom Ecommerce Chatbot",
       snapshot: chatbot,
       desc: "Built an advanced chatbot application for an ecommerce store leveraging the GPT API. Engineered custom prompts to enhance conversational abilities and provide tailored product recommendations",
-      link: "google.com",
-      code: "github.com",
+      code: "https://github.com/mzafarr/CustomerSupport-Chatbot",
+      link: "https://customer-support-chatbot-flax.vercel.app/",
+      side: 1,
+    },
+    {
+      heading: "Tesla Landing Page",
+      snapshot: tesla,
+      desc: "Built Tesla's landing page using React.js and styled components library and added animations for a responsive and beautiful design.",
+      code: "",
+      link: "https://zafars-tesla-clone.netlify.app/",
       side: 0,
     },
   ];
   return (
-    <section id="projects" className="px-4 mx-auto flex flex-col justify-center items-center bg-[#eae8e8dc] py-14">
+    <section
+      id="projects"
+      className="px-4 mx-auto flex flex-col justify-center items-center bg-[#eae8e8dc] py-14"
+    >
       <h2 className="text-5xl pb-3 font-semibold">Projects</h2>
       <div>
         {obj.map((item, index) => (
