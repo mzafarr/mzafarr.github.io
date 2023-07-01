f57573918c49c3a70d683fb6e0e4e1168d3cf621
Muhammad Zafar <mzafar611@gmail.com>
2023-07-01 10:09:21 +0500

    commit

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -28,16 +28,28 @@ const Project: FC<IProject> = ({
           <p className="text-lg ">{desc}</p>
         </div>
         <div className="flex flex-col w-full">
-          <button className="px-6 py-3 text-center bg-violet-700 hover:bg-violet-600 text-white mt-5  mb-1.5 ml-0 rounded-lg">
-            <a target="_blank" className="relative top-0 left-0 w-full h-full text-decoration-none text-inherit" href={link}>View Project</a>
-          </button>
-          <button className="px-6 py-3 text-center bg-slate-700 hover:bg-slate-600 text-white  mt-2 rounded-lg">
-            <a target="_blank" className="relative top-0 left-0 w-full h-full text-decoration-none text-inherit" href={code}>Source Code</a>
-          </button>
+          <a
+            target="_blank"
+            className="relative top-0 left-0 w-full h-full text-decoration-none text-inherit"
+            href={link}
+          >
+            <button className="px-6 py-3 text-center bg-violet-700 hover:bg-violet-600 text-white mt-5  mb-1.5 ml-0 rounded-lg">
+              View Project
+            </button>
+          </a>
+          <a
+            target="_blank"
+            className="relative top-0 left-0 w-full h-full text-decoration-none text-inherit"
+            href={code}
+          >
+            <button className="px-6 py-3 text-center bg-slate-700 hover:bg-slate-600 text-white  mt-2 rounded-lg">
+              Source Code
+            </button>
+          </a>
         </div>
       </div>
       <div className="flex-1 mx-auto">
-        <Image src={snapshot} alt={heading}  />
+        <Image src={snapshot} alt={heading} />
       </div>
     </div>
   );
